be0e6c66c37c2e6bde20dd89aeb010ca01a30ea4
PassakornSaeng10 <s40575@bj.ac.th>
2025-01-03 19:58:46 +0700

    Add files via upload

---
+++ Midterm_10/offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1735908916,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/player-sheet0.png",
+		"images/coin-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/sprite-sheet1.png",
+		"images/ground.png",
+		"images/trueplayer-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"media/mario jump sound effect.m4a",
+		"media/mario jump sound effect.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
